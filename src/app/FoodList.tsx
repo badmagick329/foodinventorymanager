cd6ed31be0d94fea2bd53d5da3446936e9816653
badmagick <uzairfarooqi@gmail.com>
2023-10-16 18:41:43 +0100

    add error page

--- src/app/FoodList.tsx
+++ src/app/FoodList.tsx
@@ -39,25 +39,30 @@ export default async function FoodList() {
   return (
     <>
       <Suspense
-        fallback={
-          <tr>
-            <td>Loading...</td>
-            <td>...</td>
-            <td>...</td>
-            <td>...</td>
-          </tr>
-        }
+        fallback={<span className="text-2xl font-semibold">Loading...</span>}
       >
-        {foods.map((food: Food) => (
-          <tr className="hover:bg-slate-900" key={food.id}>
-            <FoodName id={food.id} name={food.name} />
-            <FoodAmount id={food.id} amount={food.amount} />
-            <FoodUnit id={food.id} unit={food.unit} />
-            <td className="flex space-x-2">
-              <FoodRemove id={food.id} />
-            </td>
-          </tr>
-        ))}
+        <table className="table table-bordered md:w-1/2">
+          <thead className="text-xl text-white">
+            <tr>
+              <th>Name</th>
+              <th>Amount</th>
+              <th>Unit</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {foods.map((food: Food) => (
+              <tr className="hover:bg-slate-900" key={food.id}>
+                <FoodName id={food.id} name={food.name} />
+                <FoodAmount id={food.id} amount={food.amount} />
+                <FoodUnit id={food.id} unit={food.unit} />
+                <td className="flex space-x-2">
+                  <FoodRemove id={food.id} />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </Suspense>
     </>
   );
