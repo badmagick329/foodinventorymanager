aadb74ae2d8b13f87179c5da1462743b7aefe5c5
badmagick <uzairfarooqi@gmail.com>
2023-10-16 19:32:35 +0100

    test modal

---
+++ src/app/@modal/(.)test/page.tsx
@@ -0,0 +1,11 @@
+import Modal from "@/components/modal/Modal";
+
+export default function TestModal() {
+  return (
+    <>
+      <Modal>
+        <span className="text-2xl font-semibold">Test modal</span>
+      </Modal>
+    </>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
-import FoodList from "./FoodList";
+import Link from "next/link";
+import FoodList from "@/components/FoodList";
 
 export default function Home() {
   return (
     <>
+      <Link href="/test">Click me</Link>
       <FoodList />
     </>
   );
