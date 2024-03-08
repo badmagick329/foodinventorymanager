a95ca77184478440a3a20fcb8f77c74862eb34dd
badmagick <uzairfarooqi@gmail.com>
2024-03-08 02:16:27 +0000

    feat: persist storage filter state

--- src/app/_components/food-list.tsx
+++ src/app/_components/food-list.tsx
@@ -6,23 +6,20 @@ import SearchBar from "./search-bar";
 import StorageFilters from "./storage-filters";
 import { StorageFiltersType } from "@/lib/types";
 import FoodCards from "./food-cards";
+import useLocalStorage from "@/hooks/useLocalStorage";
 
 export default function FoodList({ foods }: { foods: Food[] | null }) {
   const [searchText, setSearchText] = useState("");
-  const [storageFilters, setStorageFilters] = useState(generateStorageState);
+  const [value, updateValue] = useLocalStorage(
+    "storageFilters",
+    generateStorageState()
+  );
 
   return (
     <div className="flex flex-col w-full items-center gap-4">
       <SearchBar searchText={searchText} setSearchText={setSearchText} />
-      <StorageFilters
-        storageFilters={storageFilters}
-        setStorageFilters={setStorageFilters}
-      />
-      <FoodCards
-        foods={foods}
-        searchText={searchText}
-        storageFilters={storageFilters}
-      />
+      <StorageFilters storageFilters={value} setStorageFilters={updateValue} />
+      <FoodCards foods={foods} searchText={searchText} storageFilters={value} />
     </div>
   );
 }
