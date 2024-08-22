bb5b5480c6e526b86597307411ec7cf6388422f1
badmagick329 <uzairfarooqi@gmail.com>
2024-08-22 22:11:24 +0100

    feat: shopping list sorting

--- src/app/shopping/_components/shopping-list.tsx
+++ src/app/shopping/_components/shopping-list.tsx
@@ -11,18 +11,21 @@ interface ShoppingListProps {
 
 export default function ShoppingList({ shoppingItems }: ShoppingListProps) {
   const [item, setItem] = useState("");
+  shoppingItems = shoppingItems || [];
 
   return (
     <div className="flex flex-col items-start p-2 w-full sm:w-3/4 lg:w-1/2">
-      {shoppingItems?.map((item, idx) => (
-        <div
-          key={item.id}
-          className="flex w-full p-2 justify-between items-center"
-        >
-          <ShoppingItemDisplay item={item} index={idx} />
-          <RemoveButton id={item.id} removeCallback={removeShoppingItem} />
-        </div>
-      ))}
+      {shoppingItems
+        .toSorted((a, b) => a.id - b.id)
+        .map((item, idx) => (
+          <div
+            key={item.id}
+            className="flex w-full p-2 justify-between items-center"
+          >
+            <ShoppingItemDisplay item={item} index={idx} />
+            <RemoveButton id={item.id} removeCallback={removeShoppingItem} />
+          </div>
+        ))}
       <form
         className="flex w-full p-2 space-x-2"
         action={(e) => {
